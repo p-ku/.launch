f3a375dd76213b70d5ecd37990fed5b7a67548b5
puerileYam <66377062+puerileYam@users.noreply.github.com>
2020-08-18 14:48:17 +0000

    Formatting

--- init.sh
+++ init.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 apt full-upgrade
 apt install git vim tmux
+
 # Copied from https://missing.csail.mit.edu/2020/editors/
 # "Many programs use the GNU Readline library for their command-line interface."
 # "Readline supports (basic) Vim emulation too, which can be enabled by adding the following line to the ~/.inputrc file:"
 echo "set editing-mode vi" >> ~/.inputrc
+
 # Add some basic Vim plugins, recommended by the MIT missing semester class
 mkdir -p ~/.vim/pack/vendor/start
 cd ~/.vim/pack/vendor/start
