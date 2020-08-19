e0226c70c187b127877cf63eb30200c9edfc0a12
p-ku <contact@p-ku.com>
2020-08-19 15:07:32 +0000

    Only use sudo when necessary

--- init.sh
+++ init.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
+
+real_user=$SUDO_USER
+
 apt -y full-upgrade
 apt -y install git vim tmux zsh
-
-
 
 # Copied from https://missing.csail.mit.edu/2020/editors/
 # "Many programs use the GNU Readline library for their command-line interface."
@@ -10,23 +11,23 @@ apt -y install git vim tmux zsh
 echo "set editing-mode vi" >> ~/.inputrc
 
 # Add some basic Vim plugins, recommended by the MIT missing semester class
-mkdir -p ~/.vim/pack/vendor/start
-cd ~/.vim/pack/vendor/start
-git clone https://github.com/ctrlpvim/ctrlp.vim
-git clone https://github.com/mileszs/ack.vim
-git clone https://github.com/scrooloose/nerdtree
-git clone https://github.com/easymotion/vim-easymotion
+sudo -u $real_user mkdir -p ~/.vim/pack/vendor/start
+sudo -u $real_user cd ~/.vim/pack/vendor/start
+sudo -u $real_user git clone https://github.com/ctrlpvim/ctrlp.vim
+sudo -u $real_user git clone https://github.com/mileszs/ack.vim
+sudo -u $real_user git clone https://github.com/scrooloose/nerdtree
+sudo -u $real_user git clone https://github.com/easymotion/vim-easymotion
 # With this setting, for example, the Python REPL will support Vim bindings.
 
-zsh
+sudo -u $real_user zsh
 
-git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+sudo -u $real_user git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 
-ln -s ~/dotfiles/vimrc ~/.vimrc
-ln -s ~/dotfiles/tmux.config ~/.tmux.config
-ln -s ~/dotfiles/zdot/zshenv ~/.zshenv
-ln -s ~/dotfiles/zdot/zprofile ~/.zprofile
-ln -s ~/dotfiles/zdot/zshrc ~/.zshrc
-ln -s ~/dotfiles/zdot/zlogin ~/.zlogin
-ln -s ~/dotfiles/zdot/zlogout ~/.zlogout
-ln -s ~/dotfiles/zdot/zpreztorc ~/.zpreztorc
+sudo -u $real_user ln -s ~/dotfiles/vimrc ~/.vimrc
+sudo -u $real_user ln -s ~/dotfiles/tmux.config ~/.tmux.config
+sudo -u $real_user ln -s ~/dotfiles/zdot/zshenv ~/.zshenv
+sudo -u $real_user ln -s ~/dotfiles/zdot/zprofile ~/.zprofile
+sudo -u $real_user ln -s ~/dotfiles/zdot/zshrc ~/.zshrc
+sudo -u $real_user ln -s ~/dotfiles/zdot/zlogin ~/.zlogin
+sudo -u $real_user ln -s ~/dotfiles/zdot/zlogout ~/.zlogout
+sudo -u $real_user ln -s ~/dotfiles/zdot/zpreztorc ~/.zpreztorc
