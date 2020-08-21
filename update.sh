f605c0f1386c7054bfbaf5280e8fbaba1b95d07b
p-ku <contact@p-ku.com>
2020-08-21 21:51:02 +0000

    Apply shellcheck suggestions
    Add double quotes to variables, add failsafe exit after cd

--- update.sh
+++ update.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 apt -y update; apt -y upgrade
 
-cd /home/$SUDO_USER/.vim/pack/vendor/start
+cd /home/"$SUDO_USER"/.vim/pack/vendor/start || exit
 
 find . -maxdepth 1 -type d -exec sh -c '(cd {} && git pull)' ';'
 
-cd /home/$SUDO_USER/.zprezto
+cd /home/"$SUDO_USER"/.zprezto || exit
 
 git pull
 git submodule update --init --recursive
 
-DOTNAME=$(ls /home/$SUDO_USER/.launch/dots)
+DOTNAME=$(ls /home/"$SUDO_USER"/.launch/dots)
 for dotfile in $DOTNAME; do
-  ln -sfn /home/$SUDO_USER/.launch/dots/$dotfile /home/$SUDO_USER/."$dotfile"
+  ln -sfn /home/"$SUDO_USER"/.launch/dots/"$dotfile" /home/"$SUDO_USER"/."$dotfile"
 done
 
-cd /home/$SUDO_USER
+cd /home/"$SUDO_USER" || exit
