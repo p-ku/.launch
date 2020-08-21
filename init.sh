518e94a01b44de34d9b6fbed513b3156bfc82fb6
p-ku <contact@p-ku.com>
2020-08-21 21:41:58 +0000

    Apply shellcheck suggestions
    add || exit when cd is called, add double quotes to variables

--- init.sh
+++ init.sh
@@ -4,12 +4,12 @@ apt -y update; apt -y upgrade
 # Copied from https://missing.csail.mit.edu/2020/editors/
 # "Many programs use the GNU Readline library for their command-line interface."
 # "Readline supports (basic) Vim emulation too, which can be enabled by adding the following line to the ~/.inputrc file:"
-echo "set editing-mode vi" >> /home/$SUDO_USER/.inputrc
+echo "set editing-mode vi" >> /home/"$SUDO_USER"/.inputrc
 
 # Add some basic Vim plugins, recommended by the MIT missing semester class
-mkdir -p /home/$SUDO_USER/.vim/pack/vendor/start
+mkdir -p /home/"$SUDO_USER"/.vim/pack/vendor/start
 
-cd /home/$SUDO_USER/.vim/pack/vendor/start
+cd /home/"$SUDO_USER"/.vim/pack/vendor/start || exit
 
 git clone https://github.com/ctrlpvim/ctrlp.vim.git
 vim -u NONE -c "helptags ctrlp.vim/doc" -c q
@@ -21,9 +21,8 @@ git clone https://github.com/easymotion/vim-easymotion.git
 vim -u NONE -c "helptags vim-easymotion/doc" -c q
 git clone https://tpope.io/vim/fugitive.git
 vim -u NONE -c "helptags fugitive/doc" -c q
-# With this setting, for example, the Python REPL will support Vim bindings.
 
-cd /home/$SUDO_USER
+cd /home/"$SUDO_USER" || exit
 
 git clone --recursive https://github.com/sorin-ionescu/prezto.git ./.zprezto
 
