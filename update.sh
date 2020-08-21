326b42084eb2898ce0e0743923449cf83722bbf7
p-ku <contact@p-ku.com>
2020-08-21 21:56:12 +0000

    Change git pull method for extensions to a loop
    This method does not trigger shellcheck errors. Previous method was considered "fragile", injected filepaths could be exploited apparently.

--- update.sh
+++ update.sh
@@ -3,7 +3,11 @@ apt -y update; apt -y upgrade
 
 cd /home/"$SUDO_USER"/.vim/pack/vendor/start || exit
 
-find . -maxdepth 1 -type d -exec sh -c '(cd {} && git pull)' ';'
+REPNAME=$(ls /home/"$SUDO_USER"/.vim/pack/vendor/start)
+for repos in $REPNAME; do
+  cd /home/"$SUDO_USER"/.vim/pack/vendor/start/"$repos" || exit
+  git pull
+done
 
 cd /home/"$SUDO_USER"/.zprezto || exit
 
